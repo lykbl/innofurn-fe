35bedccc77049368b2e16951f74ed5395d675e79
rkpiii <deslike33@gmail.com>
2023-11-26 19:37:38 +0100

    add animated accordion, slideout menu and subheader

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/styles/global.scss';
 import Header from '@/app/ui/storefront/layout/header';
+import React from "react";
+import Subheader from "@/app/ui/storefront/layout/subheader";
 
 export default function RootLayout({
   children,
@@ -11,7 +13,7 @@ export default function RootLayout({
       <body className='antialiased text-base flex items-center flex-col'>
         <header className='w-full'>
           <Header />
-          {/*<FeaturedLinks />*/}
+          <Subheader />
         </header>
         <main className='max-w-screen-2xl w-full mx-auto border-2 border-t-0'>
           {children}
---
+++ app/ui/storefront/layout/subheader.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/app/ui/common/button';
+import CategoriesMenu from '@/app/ui/storefront/layout/categories-menu';
+import {ReactNode} from "react";
+
+const promotions = [
+  {
+    id: 1,
+    name: 'Ongoing Promotions',
+    link: '/cat/furniture',
+  },
+  {
+    id: 2,
+    'name': 'Work With Us',
+    'link': '/cat/kitchen',
+  },
+  {
+    id: 3,
+    'name': 'Room Planner',
+    'link': '/room-planner',
+  }
+];
+
+export default function Subheader(): ReactNode {
+  return (
+    <div className='border-b-2'>
+      <div className='flex gap-2 py-2 px-4 max-w-screen-2xl mx-auto justify-between'>
+        <div className='flex items-center'>
+          <CategoriesMenu />
+          {promotions.map(promotion =>
+            <div key={promotion.id}>
+              <Button className='font-medium mx-2'>
+                {promotion.name}
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
