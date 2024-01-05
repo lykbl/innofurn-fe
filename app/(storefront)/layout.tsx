c524b45fdef991d1cda8c4f0dc07ff367368e664
lykbl <deslike33@gmail.com>
2024-01-05 23:28:58 +0100

    basic apollo setup

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -4,6 +4,39 @@ import React from 'react';
 import Subheader from '@/app/ui/storefront/layout/subheader';
 import Footer from '@/app/ui/storefront/layout/footer';
 import { inter } from "@/app/ui/fonts";
+import { getClient } from "@/app/lib/apollo-client";
+
+import { gql } from "@apollo/client";
+import { ApolloWrapper } from "@/app/lib/apollo-provider";
+
+const query = gql`
+    query rootCollections {
+        rootCollections {
+            id
+        }
+    }
+`;
+
+const Collections = async () => {
+  const rootCollections = await getClient().query({
+    query,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 30 },
+      },
+    },
+  });
+  console.log(rootCollections.data.rootCollections)
+
+  return (
+    <div>
+      Collections!
+      {rootCollections.data.rootCollections.map((collection: any) =>
+        <div>{collection.name}</div>
+      )}
+    </div>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -16,16 +49,19 @@ export default function RootLayout({
       className='h-screen'
     >
     <body className={`antialiased text-base flex items-center flex-col ${inter.className}`}>
-    <header className='w-full'>
-      <Header />
-      <Subheader />
-    </header>
-    <main className='max-w-screen-2xl w-full px-2 mx-auto'>
-      {children}
-    </main>
-    <footer className='py-16 bg-neutral-100 w-full mt-8'>
-      <Footer />
-    </footer>
+      <ApolloWrapper>
+        <header className='w-full'>
+          <Header />
+          <Subheader />
+          <Collections />
+        </header>
+        <main className='max-w-screen-2xl w-full px-2 mx-auto'>
+          {children}
+        </main>
+        <footer className='py-16 bg-neutral-100 w-full mt-8'>
+          <Footer />
+        </footer>
+      </ApolloWrapper>
     </body>
     </html>
   );
--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,6 +1,30 @@
+'use client';
+
 import Link from "@/app/ui/common/link";
+// import { useSuspenseQuery } from "@apollo/client"; //TODO REVERT?
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+// import { gql } from "@/app/__generated__";
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+
+const GET_COLLECTIONS = gql(/* GraphQL */`
+    query GetRootCollections {
+        rootCollections {
+            id
+        }
+    }
+`);
 
 export default function Page() {
+  const { data } = useSuspenseQuery(
+    GET_COLLECTIONS as any,
+    {
+      variables: {},
+    }
+  );
+
+  console.log(data);
+
   return (
     <>
       Main page
