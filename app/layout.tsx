66e84ab005b1387e276d9385d1598f0b657f3420
lykbl <deslike33@gmail.com>
2024-02-10 22:47:44 +0100

    make body take 100% height

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-screen">
+    <html lang="en">
       <body
         className={cn(
           "antialiased text-base flex items-center flex-col",
